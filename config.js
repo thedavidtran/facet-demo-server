b4d7f184d52f0a266f97aa5c47314f5a837d5abe
thedavidtran <david@davidtran.io>
2021-06-14 14:43:15 -0400

    Add support for process env CLIENT_REACT_URL

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const DEFAULT_MONGODB_URI = "mongodb+srv://admin:admin@facet-east.dipeq.mongodb.net/facet?retryWrites=true&w=majority";
 module.exports = {
 	corsConfig: { // cors library
-		origin: "http://localhost:8081"
+		origin: process.env.CLIENT_REACT_URI || "http://localhost:8081"
 	},
 	dbConfig: { // configuration for MongoDB atlas cluster
 		url: process.env.MONGODB_URI || DEFAULT_MONGODB_URI,
